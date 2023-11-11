e78dd8b9463466527309f407be6b3393de7aeebe
rrhan32 <96813144+rrhan32@users.noreply.github.com>
2023-11-12 04:04:57 +0530

    new props added

--- src/components/CardSample.tsx
+++ src/components/CardSample.tsx
@@ -5,8 +5,13 @@ import Grid from '@mui/material/Grid';
 import MediaCard from "./MediaCard";
 
 function CardSample(props:any) {
-  const {onCardsClick}=props
+  const {onCardsClick,product_api}=props
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+// Generate a random index within the array's bounds
+
+// Use the random index to get a random number from the array
+const randomValue = numbers[Math.floor(Math.random() * numbers.length)];
   const Item = styled(Paper)(() => ({
     backgroundColor: '#98d6a9',
     padding: 8,
@@ -19,27 +24,27 @@ function CardSample(props:any) {
       <Grid container spacing={6}>
 
         <Grid item xs={4}>
-          <MediaCard onCardClick={onCardsClick} />
+          <MediaCard onCardClick={onCardsClick} produce_api={product_api} randomVal={0}/>
         </Grid>
 
         <Grid item xs={4}>
-          <MediaCard onCardClick={onCardsClick}/>
+          <MediaCard onCardClick={onCardsClick} produce_api={product_api} randomVal={1}/>
         </Grid>
 
         <Grid item xs={4}>
-          <MediaCard onCardClick={onCardsClick}/>
+          <MediaCard onCardClick={onCardsClick} produce_api={product_api} randomVal={2}/>
         </Grid>
 
         <Grid item xs={4}>
-          <MediaCard onCardClick={onCardsClick}/>
+          <MediaCard onCardClick={onCardsClick} produce_api={product_api} randomVal={3}/>
         </Grid>
 
         <Grid item xs={4}>
-          <MediaCard onCardClick={onCardsClick}/>
+          <MediaCard onCardClick={onCardsClick} produce_api={product_api} randomVal={2}/>
         </Grid>
 
         <Grid item xs={4}>
-          <MediaCard onCardClick={onCardsClick}/>
+          <MediaCard onCardClick={onCardsClick} produce_api={product_api} randomVal={1}/>
         </Grid>
 
       </Grid>
